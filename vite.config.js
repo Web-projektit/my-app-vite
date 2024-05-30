9c35042927a44070c2cab59190b693e3eb3e058c
Web-projektit <jukka.aula@omnia.fi>
2024-05-30 12:10:57 +0300

    palautettu fetch credentials

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { copy } from 'vite-plugin-copy'
+//import { copy } from 'vite-plugin-copy'
 /* Huom. base määrittää aloituspolun 
    React-kehityspalvelin: ''
    XAMPP: react-sovellusmalli-ii
 */
 
 export default defineConfig({
-  plugins: [react(),
-            copy({
-              targets: [
-                { src: 'public/.htaccess', dest: 'c:/xampp/htdocs/react-sovellusmalli-ii' }
-              ],
-              hook: 'writeBundle' // run the plugin after the bundle is written
-            })],
-  base: 'react-sovellusmalli-ii',
+  plugins: [react()],
+  base: '',
   build: {
-    outDir: 'c:/xampp/htdocs/react-sovellusmalli-ii',
+    outDir: 'dist',
     assetsDir: 'static'
   }
 })
